812bbf2e1037a5f315d4af847974edd1b7306c73
hamissa nicole <nsabihamiss@gmail.com>
2020-09-14 16:23:20 +0200

    update ussd

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns=[
     path('',views.welcome, name='home'),
+    path('product/',views.Product, name='product'),
+    path('ussdapp/',views.ussdapp, name='ussdapp'),
     
     
     
--- home/views.py
+++ home/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-
+import africastalking
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+username = "nsabihamiss@gmail.com"
+api_key = "7a6e5b772d94fc4af67cb8e9ac0b656af8ab659b27e422c2a1105d374bcb4662"
+africastalking.initialize(username,api_key)
 # Create your views here.
 def welcome(request):
     return render(request,'index.html')
 def Product(request): 
     return render(request,'Product.html') 
+
+@csrf_exempt
+def ussdapp(request):
+    if request.method == 'POST':
+        ## mandatory
+        session_id = request.POST.get("sessionid")
+        service_code = request.POST.get("servicecode")
+        phone_number =request.POST.get("phonenumber")
+        text = request.POST.get("text")
+        level = text.split('*')
+        response =""
+
+        if text =='':
+            response ="CON Welcome to ida technology USSD app \n"
+            response +="1. Girls in code \n"
+            response +="2. sdf program"
+        else:
+            response ="END invalid choice"    
+
+
+        return HttpResponse(response)
+
+    
+
+    return HttpResponse('welcome')
