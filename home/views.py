006afc6991edde92f535fa84ab2bb54170eaaa0e
bonic14 <nsabihamiss@gmail.com>
2021-04-01 10:31:59 +0200

    change

--- home/views.py
+++ home/views.py
@@ -64,11 +64,11 @@ def ussdapp(request):
 
         elif text == '1*1*1':
             response ="CON Enter your name \n"
-        elif numb =='1*1*2' and int(len(level))==3 and str(level[2]) in str(level):
+        elif numb =='1*1*1' and int(len(level))==3 and str(level[2]) in str(level):
             response ="CON Enter your Age "
-        elif numb =='1*1*3' and  int(len(level))==4 and str(level[3]) in str(level):
+        elif numb =='1*1*1' and  int(len(level))==4 and str(level[3]) in str(level):
             response ="CON Enter your phone number"
-        elif numb =='1*1*4' and  int(len(level))==5 and str(level[4]) in str(level):
+        elif numb =='1*1*1' and  int(len(level))==5 and str(level[4]) in str(level):
             response ="CON Enter your District"
 
           #=========================RTN==========
